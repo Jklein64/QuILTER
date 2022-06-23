58d1187816b885a40fdeef6b71cc28f1dec7ca2c
Jason Klein <jklein.64@verizon.net>
2022-06-23 16:10:00 -0400

    Initial commit

---
+++ guided_filter.py
@@ -0,0 +1,59 @@
+import numpy as np
+from PIL import Image
+
+# determined through experimentation
+GUIDED_FILTER_RADIUS = 30
+# taken from example on GitHub
+GUIDED_FILTER_EPSILON = 1e-6 * 255 ** 2
+
+def main():
+    # loads everything as 8-bit images
+    image = np.array(Image.open("original.png"))
+    masks = [np.array(Image.open(f"{i}.png")) for i in (1, 2, 3)]
+    filtered = filter(np.stack(masks, axis=-1), image)
+
+    union = np.sum(filtered, axis=-1)
+    # prefer sparsity
+    # subtract from smallest channel
+    # add to largest channel
+
+    # absolute value to keep as uint8; direction is from union > 255
+    difference = np.absolute(union.astype(int) - 255).astype(np.uint8)
+
+    # handle where the sum of the masks is too large
+    smallest = np.argmin(filtered[union > 255], axis=1)
+    # subtract from smallest channel to increase sparsity
+    filtered[union > 255, smallest] -= difference[union > 255]
+
+    # handle where the sum of the masks is too small
+    largest = np.argmax(filtered[union > 255], axis=-1)
+    # add to largest channel to increase sparsity
+    filtered[union < 255, largest] += difference[union < 255]
+
+    pass
+
+
+def show(image):
+    Image.fromarray(image).show()
+
+
+def filter(image, guide):
+    from cv2.ximgproc import guidedFilter
+    return guidedFilter(guide, image, GUIDED_FILTER_RADIUS, GUIDED_FILTER_EPSILON)
+
+
+def apply_mask(image, mask):
+    return np.insert(image[..., 0:3], 3, mask, axis=-1)
+
+
+if __name__ == "__main__":
+    main()
+
+# it turns out that part of the issue is also integer overflow.
+# in addition to having parts of the image that weren't fully
+# covered by the union of the masks, there were others that 
+# were over-covered by the union.  Integer overflow made it
+# look like these areas weren't covered at all :O
+
+# where s is the sum of the filtered array
+# show((np.where(s > 255, s-255, 0) * 255/28).astype(np.uint8))
