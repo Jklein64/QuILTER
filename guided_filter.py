294a3bbd4bdf451dd69fbcc1f3138ee32364bd2f
Jason Klein <jklein.64@verizon.net>
2022-06-24 02:06:28 -0400

    Distribute remaining differences.  But is it needed?

--- guided_filter.py
+++ guided_filter.py
@@ -23,10 +23,27 @@ def main():
     union = np.sum(scaled, axis=-1)
     # absolute value to keep as uint8; direction is from union > 255
     difference = np.absolute(union.astype(int) - 255).astype(np.uint8)
-    # create array to add the difference to the largest entry
-    addend = np.where(ascending_i[union < 255] == len(masks) - 1, difference[union < 255, np.newaxis], 0)
-    # TODO doesn't currently account for integer overflow. is that even possible?
-    scaled[union < 255] += addend
+
+    def add_difference(n, difference):
+        """Add the given difference to the n'th largest channel.  If a value in the channel would integer overflow (for an 8-bit integer), then it it set to the maximum value instead.  Call this function but with an increased value of n and an updated difference array until there is no longer anything to add.  Solves the note above."""
+        # get the index for the n'th largest channel
+        channel = len(masks) - n - 1
+        can_add_whole = 255 - scaled[union < 255, ascending_i[union < 255, ..., channel]] >= difference[union < 255]
+        # add the entire difference to values that can take it without overflowing
+        selection = (ascending_i[union < 255] == n) & can_add_whole[:, np.newaxis]  
+        scaled[union < 255] += np.where(selection, difference[union < 255, np.newaxis], 0)
+        # add as much as possible to ones that would overflow; equivalently, set them to the max
+        scaled[union < 255][~can_add_whole] = 255
+
+    i = 0
+    while not np.all(difference == 0):
+        add_difference(i, difference)
+        union = np.sum(scaled, axis=-1)
+        difference = np.absolute(union.astype(int) - 255).astype(np.uint8)
+        i += 1
+
+    if i == 1:
+        print("finished on the first iteration.  Do you really need the repetitive process?")
 
     pass
 
