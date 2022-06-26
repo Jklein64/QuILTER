c89d56c463f28c014f2d190c1ed14925edc68f01
Jason Klein <jklein.64@verizon.net>
2022-06-26 01:07:08 -0400

    Create CLI

--- guided_filter.py
+++ guided_filter.py
@@ -1,43 +1,47 @@
-import numpy as np
+from argparse import ArgumentParser
 from PIL import Image
+
+import numpy as np
 import os
-
-from time import perf_counter
-
-# determined through experimentation
-GUIDED_FILTER_RADIUS = 30
-# taken from example on GitHub
-GUIDED_FILTER_EPSILON = 1e-6 * 255 ** 2
 
 
 def main():
+    parser = ArgumentParser(description="Feather binary masks from semantic segmentation using a guided filter without affecting their union.")
+    parser.add_argument("image", help="Path to the original image file")
+    parser.add_argument("masks", help="Path to a directory containing binary masks for each segment; the union of the masks covers the whole image")
+    parser.add_argument("-r", "--radius", default=8, help="Guided filter window radius, default is 8")
+    parser.add_argument("-e", "--epsilon", default=1e-6, help="Guided filter epsilon, default is 1e-6")
+    parser.add_argument("-o", "--output", help="Output directory for the feathered masks. Not supplying this argument will show them instead")
+    args = parser.parse_args()
+
     # loads everything as 8-bit, 3-channel images
-    image = np.array(Image.open("car-kids/car-kids.png"))[..., 0:3]
-    masks = [np.array(Image.open(f"car-kids/masks/{file}")) for file in os.listdir("car-kids/masks")]
+    image = np.array(Image.open(args.image))[..., 0:3]
+    masks = [np.array(Image.open(os.path.join(args.masks, file))) for file in os.listdir(args.masks)]
 
-    start = perf_counter()
-
-    filtered = filter(np.stack(masks, axis=-1), image).astype(int)
-
-    # scale to keep the ratio between masks but have a max sum of 255
+    # store masks as if they were per-pixel features
+    filtered = filter(np.stack(masks, axis=-1), image, args.radius, args.epsilon).astype(int)
+    # scale to keep the ratio between mask opacities but have a max sum of 255
     scaled = (255 * filtered / np.sum(filtered, axis=-1, keepdims=True)).astype(np.uint8)
-    ascending_indices = np.argsort(scaled, axis=-1)
     union = np.sum(scaled, axis=-1)
     # absolute value to keep as uint8; direction is from union > 255
     difference = np.absolute(union.astype(int) - 255).astype(np.uint8)
     # expand dims to add the difference to the largest value
+    ascending_indices = np.argsort(scaled, axis=-1)
     largest_value = ascending_indices[union < 255] == len(masks) - 1
     difference = np.where(largest_value, difference[union < 255, np.newaxis], 0)
     # np.all(np.sum(scaled, axis=-1) == 255) should be True after this addition
     scaled[union < 255] += difference
 
-    end = perf_counter()
-    print(f"took {end - start} seconds to complete the operation.")
-
-    pass
+    for i in range(len(masks)):
+        output = Image.fromarray(scaled[..., i])
+        if args.output:
+            output.save(args.output)
+        else:
+            output.show()
 
 
 def show(image):
+    """Helper function for debugging"""
     Image.fromarray(image).show()
 
 
@@ -56,9 +60,9 @@ def pca(data, dim):
     return projected
 
 
-def filter(image, guide):
+def filter(image, guide, radius, epsilon):
     from cv2.ximgproc import guidedFilter
-    return guidedFilter(guide, image, GUIDED_FILTER_RADIUS, GUIDED_FILTER_EPSILON)
+    return guidedFilter(guide, image, radius, epsilon)
 
 
 def apply_mask(image, mask):
