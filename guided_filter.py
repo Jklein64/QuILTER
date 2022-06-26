2dece4f7d4783ea4c1d53965c798a862b6dc5fe2
Jason Klein <jklein.64@verizon.net>
2022-06-25 23:29:45 -0400

    Rename things and add comments

--- guided_filter.py
+++ guided_filter.py
@@ -12,8 +12,8 @@ GUIDED_FILTER_EPSILON = 1e-6 * 255 ** 2
 
 def main():
     # loads everything as 8-bit, 3-channel images
-    image = np.array(Image.open("horse/horse.png"))[..., 0:3]
-    masks = [np.array(Image.open(f"horse/masks/{file}")) for file in os.listdir("horse/masks")]
+    image = np.array(Image.open("car-kids/car-kids.png"))[..., 0:3]
+    masks = [np.array(Image.open(f"car-kids/masks/{file}")) for file in os.listdir("car-kids/masks")]
 
     start = perf_counter()
 
@@ -21,12 +21,12 @@ def main():
 
     # scale to keep the ratio between masks but have a max sum of 255
     scaled = (255 * filtered / np.sum(filtered, axis=-1, keepdims=True)).astype(np.uint8)
-    ascending_i = np.argsort(scaled, axis=-1)
+    ascending_indices = np.argsort(scaled, axis=-1)
     union = np.sum(scaled, axis=-1)
     # absolute value to keep as uint8; direction is from union > 255
     difference = np.absolute(union.astype(int) - 255).astype(np.uint8)
     # expand dims to add the difference to the largest value
-    largest_value = ascending_i[union < 255] == len(masks) - 1
+    largest_value = ascending_indices[union < 255] == len(masks) - 1
     difference = np.where(largest_value, difference[union < 255, np.newaxis], 0)
     # np.all(np.sum(scaled, axis=-1) == 255) should be True after this addition
     scaled[union < 255] += difference
@@ -42,6 +42,7 @@ def show(image):
 
 
 def pca(data, dim):
+    # should this be standardized?
     mu = np.mean(data, axis=0)
     sigma = np.std(data, axis=0)
     standardized = (data - mu) / sigma
